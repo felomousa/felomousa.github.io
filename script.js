231d08c76dca4b8a90a3ec8dfece7773a0cf69f1
felomousa <felogmousa@gmail.com>
2024-02-26 14:14:22 -0800

    Addressed incorrect icon colours

--- script.js
+++ script.js
@@ -13,20 +13,19 @@ document.addEventListener('DOMContentLoaded', function () {
       el.style.color = isDarkMode ? '#8f82ff' : '#FFA30E';
     });
 
-    // Update icon colors
-    const resumeIcon = document.querySelector('.fa-file-pdf-o');
-    if (resumeIcon) {
-      resumeIcon.style.color = isDarkMode ? '#FFFFFF' : '#FFA30E'; // Change to white in dark mode, back to orange in light mode
-    }
 
     // Optionally, update other icons as needed
     const githubIcon = document.querySelector('.fa-github');
     const linkedinIcon = document.querySelector('.fa-linkedin');
+    const resumeIcon = document.querySelector('.fa-file-pdf-o');
     if (githubIcon) {
       githubIcon.style.color = isDarkMode ? '#FFFFFF' : 'initial'; // Assuming you want white in dark mode, and default in light
     }
     if (linkedinIcon) {
-      linkedinIcon.style.color = isDarkMode ? '#FFFFFF' : '#0077b5'; // LinkedIn blue in light mode, white in dark mode
+      linkedinIcon.style.color = isDarkMode ? '#0077b5' : '#0077b5';
+    }
+    if (resumeIcon) {
+      resumeIcon.style.color = isDarkMode ? '#0f1138' : '#FFA30E';
     }
   }
 
