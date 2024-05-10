6c39d020bd0a0df0bdf17aeae5fda8a797df5feb
felomousa <felogmousa@gmail.com>
2024-05-10 01:10:28 -0700

    Added media as well as project descriptions

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-document.body.style.height = '222vh';
+document.body.style.height = '250vh';
 
 document.addEventListener('scroll', function () {
     const currentScrollY = window.scrollY;
     const letterF = document.getElementById('letterF');
     const letterM = document.getElementById('letterM');
     const maxScrollThreshold = 10 * window.innerHeight / 100;
-
     let effectiveScrollY = Math.min(currentScrollY, maxScrollThreshold);
+    let transformAmountF = (effectiveScrollY / maxScrollThreshold) * 100;
+    let transformAmountM = (effectiveScrollY / maxScrollThreshold) * 159;
 
-    letterF.style.transform = `translateX(${-effectiveScrollY}vh)`;
-    letterM.style.transform = `translateX(${effectiveScrollY}vh)`;
+    letterF.style.transform = `translateX(${-transformAmountF}vh)`;
+    letterM.style.transform = `translateX(${transformAmountM}vh)`;
 });
